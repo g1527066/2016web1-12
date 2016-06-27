ac28aeb762092544bea0a67ebbf085c6711963e5
nyanko-sankaku <g1527066@st.t-kougei.ac.jp>
2016-06-27 17:24:06 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,3 +1,28 @@
 document.getElementById('form').onsubmit=function(){
   window.alert(document.getElementById('form').word.value);
 };
+
+for(var i=0;i<word;i++){
+  var li=document.createElement('li');
+
+  if(word%3===0&&word%5===0)//3.5の公倍数か判断
+  {
+    
+  }
+  else if(word%3===0)//３の倍数か判断
+  {
+    
+  }
+  else if(word%5===0)//5の倍数か判断
+  {
+    
+  }
+  else
+  {
+      li,textContent=;
+  }
+  
+  
+
+document.getElementById('list').appendChild(li);  
+}
