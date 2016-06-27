21a776a38b2a30056eadae5d8844c370d31a8a21
nyanko-sankaku <g1527066@st.t-kougei.ac.jp>
2016-06-27 17:39:55 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -3,29 +3,30 @@ document.getElementById('form').onsubmit=function(){
   window.alert(document.getElementById('form').word.value);
 };
 
-/*
+//結果を表示
+
 for(var i=0;i<word;i++){
   var li=document.createElement('li');
 
   if(word%3===0&&word%5===0)//3.5の公倍数か判断
   {
-    
+      document.getElementById('choice').textContent=FizzBuzz;
   }
   else if(word%3===0)//３の倍数か判断
   {
-    
+      document.getElementById('choice').textContent=Fizz;
   }
   else if(word%5===0)//5の倍数か判断
   {
-    
+      document.getElementById('choice').textContent=Buzz;
   }
   else
   {
-      li,textContent=;
+      document.getElementById('choice').textContent=word;
   }
   
   
 
 document.getElementById('list').appendChild(li);  
 }
-*/
+
